20fc15a321212764e89d362cacf15da0c2416d33
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-01-11 16:51:13 +0100

    adding Special Member Function

--- playground/headers/move_sem4.h
+++ playground/headers/move_sem4.h
@@ -4,8 +4,14 @@
 
 struct Foo{
 
-    // Special Member Function
-
+    // Special Member Function (if not created by user the compiler automatically create one )
+    // if you redefine onse of this you have to define the rest of it !!! (rule of 5 -> formally rule of 3 )
+    Foo() = default;                             // default constructor
+    Foo(Foo const& other) = default;             // copy constructor;
+    Foo& operator=(Foo const& other) = default;  // copy assigment operator
+    Foo(Foo&& other) = default;                  // move constructor
+    Foo& operator=(Foo&& other) = default;       // move assigment operator
+    ~Foo() = default;                            // destructor
 
     int* i = nullptr;
 };
